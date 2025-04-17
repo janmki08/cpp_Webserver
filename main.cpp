9ea027ba57c52bf4214a85dab23c009e033ee395
janmki08 <bsmp4120@gmail.com>
2025-04-17 21:27:48 +0900

    멀티 스레딩

--- main.cpp
+++ main.cpp
@@ -3,26 +3,45 @@
 #include <cstring>
 #include <unistd.h>     // POSIX 시스템 함수
 #include <netinet/in.h> // sockaddr_in, IPPROTO_TCP 등 네트워크 구조체
+#include <thread>
 
 #define PORT 8080
 
 using namespace std;
 
+// 쓰레드로 클라이언트 제어
+void handle_client(int csocket)
+{
+    // 요청 받기(read)
+    char buffer[30000] = {0};
+    int valread = read(csocket, buffer, 30000);
+    cout << "받은 요청: " << buffer << "\n";
+
+    // 응답 작성
+    string response =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/plain\r\n"
+        "Content-Length: 13\r\n"
+        "\r\n"
+        "Hello, World!";
+
+    // 응답 보내기
+    write(csocket, response.c_str(), response.length());
+    cout << "응답 보냄\n";
+
+    // 연결 종료
+    close(csocket);
+}
+
 int main()
 {
-    int server_fd;  // 연결 기다리는 소켓(listen), 파일 디스크립터
-    int new_socket; // 연결 시 생성 소켓(accept)
-    long valread;
+    int server_fd;                 // 연결 기다리는 소켓(listen), 파일 디스크립터
+    int new_socket;                // 연결 시 생성 소켓(accept)
     struct sockaddr_in address;    // 바인딩될 주소 정보 구조체
     int addrlen = sizeof(address); // 구조체 크기(accept 함수에 필요)
 
     // 소켓 생성
-    server_fd = socket(AF_INET, SOCK_STREAM, 0); // IPv4, TCP, 기본 프로토콜
-    if (server_fd == 0)
-    {
-        perror("소켓 연결 실패");
-        exit(EXIT_FAILURE);
-    }
+    server_fd = socket(AF_INET, SOCK_STREAM, 0); // IPv4, TCP(UDP는 SOCK_DGRAM), 기본 프로토콜
 
     // 주소/포트 재사용 옵션
     int opt = 1;
@@ -33,51 +52,25 @@ int main()
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT); // 호스트 바이트 오더 -> 네트웤 바이트 오더(엔디안 변환)
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
-    {
-        perror("바인드 실패");
-        exit(EXIT_FAILURE);
-    }
+    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+    listen(server_fd, 10); // 백로그 큐(동시 접속 대기열) 10개, SOMAXCONN은 4096임.
 
-    // 연결 대기(listen)
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-
-    cout << "리스닝 포트: " << PORT << "...\n";
+    cout << "다중 스레드 서버 포트: " << PORT << "...\n";
 
     // 루프 형태: 다중 요청 처리
     while (true)
     {
         // 클라이언트 연결 수락(accept)
-        // 실질적 데이터 송수신 담당
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+        if (new_socket < 0)
         {
             perror("accept");
-            exit(EXIT_FAILURE);
+            continue;
         }
 
-        // 요청 받기(read)
-        char buffer[30000] = {0};
-        valread = read(new_socket, buffer, 30000);
-        cout << "받은 요청: " << buffer << "\n";
-
-        // 응답 작성
-        string response =
-            "HTTP/1.1 200 OK\r\n"
-            "Content-Type: text/plain\r\n"
-            "Content-Length: 13\r\n"
-            "\r\n"
-            "Hello, World!";
-
-        // 응답 보내기
-        write(new_socket, response.c_str(), response.length());
-        cout << "응답 보냄\n";
-
-        // 연결 종료
-        close(new_socket);
+        // 새 스레드 생성, 클라이언트 소켓 넘김
+        thread client_thread(handle_client, new_socket);
+        client_thread.detach(); // 백그라운드에서 실행하고 소스 리턴
     }
 
     // 도달은 안 함.
